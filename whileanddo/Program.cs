47d7caf389d19507a8f32cd4ec87f7ac605012b6
opilane <opilane@HP-CZC0359NND>
2024-05-16 12:35:04 +0300

    Add project files.

---
+++ whileanddo/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace whileanddo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Printimise test");
+            int e = 0;
+            do
+            {
+                Console.WriteLine("EEEEEEEEEEEEEEEEEEE");
+                e++;
+            }
+            while (e < 20);
+
+        }
+    }
+}
